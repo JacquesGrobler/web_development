de76366f8eaa0665bcfe89c972ab07aeac089d32
Jacques Grobler <46716252+JacquesGrobler@users.noreply.github.com>
2020-06-06 16:56:26 +0200

    e-commence commit

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/jgrobler/Documents/GitHub/web_development/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ e_commerce_project/store/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Product, Cart, CartItem
+
+def home(request, category_slug=None):
+    category_page = None
+    products = None
+    if category_slug != None:
+        category_page = get_object_or_404(Category,slug=category_slug)
+        products = Product.objects.filter(category=category_page, available=True)
+    else:
+        products = Product.objects.filter(available=True)
+    return render(request, 'home.html', {'category':category_page, 'products':products})
+
+def productPage(request, category_slug, product_slug):
+    try:
+        product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+    except Exception as e:
+        raise e
+
+    return render(request, 'product.html', {'product':product})
+
+def _cart_id(request):
+    cart = request.session.session_key
+    if not cart:
+        cart = request.session.create()
+    return cart
+
+def add_cart(request, product_id):
+    product = Product.objects.get(id=product_id)
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+    except Cart.DoesNotExist:
+        cart = Cart.objects.create(
+            cart_id = _cart_id(request)
+        )
+        cart.save()
+    try:
+        cart_item = CartItem.objects.get(product=product, cart=cart)
+        cart_item.quantity += 1
+        cart_item.save()
+    except CartItem.DoesNotExist:
+        cart_item = CartItem.objects.create(
+            product = product,
+            quantity = 1,
+            cart = cart
+        )
+        cart_item.save()
+
+    return redirect('cart_detail')
